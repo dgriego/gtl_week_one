3452eab0bbc55d74d87124996ce998aa824b135c
dgriego <daniel@jackrabbitsystems.com>
2015-09-10 23:51:17 -0600

    makes changes to black jack and adds solution file

--- black_jack.rb
+++ black_jack.rb
@@ -1,125 +1,10 @@
-require 'pry'
-cards = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '2']
-suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
-DECK = cards.product(suits)
 
-def deal_cards(quantity, hand = [])
-  if hand.empty?
-    quantity.times do
-      hand << DECK.sample
-    end
-  else
-    quantity.times do
-      hand << DECK.sample
-    end
-  end
+# Helper Methods
 
-  hand
-end
-
-def display_hand(hand, player)
-  puts "#{player}'s Cards:"
-
-  hand.length.times do |i|
-    puts "#{hand[i][0]} #{hand[i][1]}"
-  end
-end
-
-def display_score(score)
-  if score.kind_of?(Array)
-    puts "Scores with Ace: #{score[0]}, #{score[1]}"
-  else
-    puts "Score: #{score}"
-  end
-end
-
-def update_score(hand)
-  values = hand.flatten.map do |v|
-    v = "10" if v =~ /[JQK]/
-    v.to_i
-  end
-
-  if hand.flatten.include?('A')
-    score = [values.inject(:+) + 1, values.inject(:+) + 11]
-  else
-    score = values.inject(:+)
-  end
-end
-
-def black_jack_or_bust?(score, player, dealer = false)
-  if !dealer
-    win_msg = "BlackJack! #{player} Wins!"
-    bust_msg = "Bust! Dealer Wins!"
-  else
-    win_msg = "Dealer has BlakJack! #{player} Loses!"
-    bust_msg = "Dealer Busts! #{player} Wins!"
-  end
-
-  if score.kind_of?(Array)
-    if score.include?(21)
-      puts win_msg
-      return true
-    elsif score[0] > 21 && score[1] > 21
-      puts bust_msg
-      return true
-    else
-      return false
-    end
-  else
-    if score == 21
-      puts win_msg
-      return true
-    elsif score > 21
-      puts bust_msg
-      return true
-    else
-      return false
-    end
-  end
-end
-
-def hit_or_stay(score)
-  if score.kind_of?(Array)
-    if score[0] <= 17 || score[1] <= 17
-      return true
-    else
-      return false
-    end
-  else
-    if score <= 17
-      return true
-    else
-      return false
-    end
-  end
-end
-
-def sanitize_score(score)
-  if score.kind_of?(Array)
-    score.keep_if { |v| v < 21 }.join.to_i
-  else
-    score
-  end
-end
-
-def compare_scores(d_score, p_score, players_name)
-  dealer_win_msg = "Dealer Wins!"
-  player_win_msg = "#{players_name} Wins!"
-  dealer_score = sanitize_score(d_score)
-  player_score = sanitize_score(p_score)
-
-  if dealer_score > player_score
-    puts dealer_win_msg
-  elsif dealer_score == player_score
-    puts dealer_win_msg
-  else
-    puts player_win_msg
-  end
-end
-
-def prompt_to_play_again
-  puts "Would you like to play again?(y/n)"
-  ready = gets.chomp
+def initialize_deck
+  cards = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '2']
+  suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+  cards.product(suits)
 end
 
 def prompt_for_name
@@ -127,67 +12,153 @@ def prompt_for_name
   gets.chomp
 end
 
-def prompt_for_hit_stay
-  puts "Would you like to Hit or Stay?(h/s)"
-  gets.chomp
+def deal(quantity, hand, deck)
+  quantity.times do
+    hand << deck.pop
+  end
 end
+
+def display_hand(hand, player)
+  puts "--------- #{player.upcase}'s Cards ------------"
+
+  hand.each do |e|
+    puts "=> #{e[0]} of #{e[1]}"
+  end
+end
+
+def calculate_total(hand) # [["D", "A"], ["S", "Q"]]
+  card_values = hand.flatten.map do |v|
+    v = "10" if v =~ /[JQK]/
+    v = "11" if v =~ /A/
+    v.to_i
+  end #["11", "3", "10"]
+
+  total = card_values.inject(:+)
+
+  # Correct for aces
+  hand.flatten.select { |e| e == "A" }.count.times do
+    total -= 10 if total > 21
+  end
+
+  total
+end
+
+def display_total(total)
+  puts "Total: #{total}"
+  puts ""
+end
+
+# Start Game
 
 puts "Welcome to BlackJack!"
 puts "Are you ready to begin?(y/n)"
 play = gets.chomp
+players_name = prompt_for_name
+puts ""
 
 while play == "y"
-  players_name = prompt_for_name
-  puts ""
+  # Deal Cards
+  deck = initialize_deck
+  deck.shuffle!
 
-  players_hand = deal_cards(2)
-  system("clear")
+  players_hand = []
+  dealers_hand = []
+  blackjack = false
+  bust = false
+
+  deal(2, players_hand, deck)
+  deal(2, dealers_hand, deck)
+
+  # Show Cards
+  system("Clear")
   display_hand(players_hand, players_name)
-  player_score = update_score(players_hand)
-  display_score(player_score)
-  player_black_jack_or_bust = black_jack_or_bust?(player_score, players_name)
+  player_total = calculate_total(players_hand)
+  display_total(player_total)
 
-  if player_black_jack_or_bust
-    play = prompt_to_play_again
-  else
-    hit_stay = prompt_for_hit_stay
+  display_hand(dealers_hand, 'Dealer')
+  dealer_total = calculate_total(dealers_hand)
+  display_total(dealer_total)
 
-    while hit_stay == "h"
-      deal_cards(1, players_hand)
-      system("clear")
-      display_hand(players_hand, players_name)
-      player_score = update_score(players_hand)
-      display_score(player_score)
+  # Check for blackjack
+  if player_total == 21
+    puts "#{players_name} hit blackjack! #{players_name} wins!"
+    next
+  elsif dealer_total == 21
+    puts "Sorry #{players_name}, dealer hit blackjack! You lose."
+    next
+  end
 
-      if black_jack_or_bust?(player_score, players_name)
-        break
-      end
+  # Player turn
+  puts "#{players_name}, it's your turn"
+  while player_total < 21
+    puts "Would you like to 1) Hit or 2) Stay?"
+    hit_or_stay = gets.chomp
 
-      hit_stay = prompt_for_hit_stay
+    if !["1", "2"].include?(hit_or_stay)
+      puts "Please Enter a 1 or 2..."
+      next
     end
 
-    if hit_stay == "s"
-      # puts ""
-      dealers_hand = deal_cards(2)
-      display_hand(dealers_hand, "Dealer")
-      dealer_score = update_score(dealers_hand)
-      display_score(dealer_score)
-      hit_stay = hit_or_stay(dealer_score)
-      # if hit_stay
-      while hit_stay
-        deal_cards(1, dealers_hand)
-        dealer_score = update_score(dealers_hand)
-        display_score(dealer_score)
-        hit_stay = hit_or_stay(dealer_score)
-        break if !hit_stay
-      end
+    if hit_or_stay == "2"
+      puts "You chose to stay..."
+      break
+    end
 
-      if !black_jack_or_bust?(dealer_score, players_name, true)
-        compare_scores(dealer_score, player_score, players_name)
+    deal(1, players_hand, deck)
+    display_hand(players_hand, players_name)
+    player_total = calculate_total(players_hand)
+    display_total(player_total)
+
+    if player_total == 21
+      blackjack = true
+      puts "#{players_name} hit blackjack! #{players_name} wins!"
+      next
+    elsif player_total > 21
+      bust = true
+      puts "Sorry #{players_name}, it looks like you busted!"
+      next
+    end
+  end
+
+  if !blackjack && !bust
+    # Dealer Turn
+    while dealer_total < 17
+      #hit
+      puts "Dealer hits..."
+      deal(1, dealers_hand, deck)
+      display_hand(dealers_hand, "Dealer")
+      dealer_total = calculate_total(dealers_hand)
+      display_total(dealer_total)
+
+      if dealer_total == 21
+        blackjack = true
+        puts "Sorry #{players_name}, dealer hit blackjack! You lose."
+        next
+      elsif dealer_total > 21
+        bust = true
+        puts "Congratulations, dealer busted! #{players_name} Wins!"
+        next
       end
     end
   end
 
-  puts ""
-  play = prompt_to_play_again
+  if !blackjack && !bust
+    # Compare Hands
+    system("clear")
+    display_hand(players_hand, players_name)
+    display_total(player_total)
+    display_hand(dealers_hand, "Dealer")
+    display_total(dealer_total)
+
+    if player_total > dealer_total
+      puts "#{players_name} Wins!"
+    elsif player_total < dealer_total
+      puts "Sorry #{players_name}, Dealer Wins!"
+    else
+      puts "Tie Game!"
+    end
+  end
+
+  puts "#{players_name}, Would you like to play again?(y/n)"
+  play = gets.chomp
 end
